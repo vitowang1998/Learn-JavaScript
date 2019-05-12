cffb30571208f2e2ce73a7ceeba3b3d63ae7b976
Haoran Wang <vitowang1998@gmail.com>
2019-05-12 19:16:26 -0400

    Create synchronicity.js

---
+++ synchronicity.js
@@ -0,0 +1,12 @@
+// Synchronous Code
+// 1. Easy to understand/Develop
+
+// Asynchronous Code
+// 1. Hard to understand/Develop
+// 2. Enhance performance
+
+// Blocking -> Synchronous
+// Non-Blocking -> Asynchronous
+
+// Hardware is non-blocking => Asynchronous
+
